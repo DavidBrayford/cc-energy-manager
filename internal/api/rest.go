98c91ffbf497b39ad6b6c6e9f9d887b81a7e213f
DavidBrayford <dbrayford@gmail.com>
2023-08-18 11:00:43 +0200

    Changed string comparison to be case insensitive

--- internal/api/rest.go
+++ internal/api/rest.go
@@ -4,10 +4,14 @@
 // license that can be found in the LICENSE file.
 package api
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func ApiPrinter(name string) error {
-	if name == "david" {
+	// case insensitive string comparison
+	if strings.EqualFold(name, "david") {
 		fmt.Printf("Hello %s\n", name)
 	} else {
 		return fmt.Errorf("wrong name %s", name)
